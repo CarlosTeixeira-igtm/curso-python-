126b5599793131abc6ec7dda3c8a43f3845b1fc0
CarlosTeixeira-igtm <carlos@hauserservices.be>
2025-02-27 09:25:18 -0300

    Add files via upload

---
+++ exemplo2.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+            
+@app.route('/nome/<nome>',methods=['GET'])
+def pegar_nome(nome):
+    return nome
+
+@app.route('/curso/<curso>', methods=['GET'])
+def pegar_curso(curso):
+    return curso
+
+@app.route('/')
+def inicio():
+    frase = "Seja bem vinso, tá funcionando, relax!"
+    return frase            
+            
+if __name__ == '__main__':
+    app.run('0.0.0.0' , debug=True)
